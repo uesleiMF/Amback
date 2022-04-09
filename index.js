25ee885e7b0ecf295ad318ec801fe666c585f6ab
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-09 16:17:54 -0300

    um

---
+++ index.js
@@ -0,0 +1,36 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
+
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const Conn = require('./conn/conn');
+// database connection
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+// buscando os dados da variavel de ambiente
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+Conn(db_url, db_user, db_pass, db_data);
+
+// inicializar o servidor http em alguma porta para podermos acessar ele.
+const port = 3001;
+app.listen(process.env.PORT || port, () => {
+  console.log(`O servidor esta rodando na porta ${port}`);
+})
+
