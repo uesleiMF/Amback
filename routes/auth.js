c555b06bc06f7d991d79c0fe074d97d7feb413ab
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-09 22:38:43 -0300

    m

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
 		const token = user.generateAuthToken();
 		res.status(200).send({ data: token, message: "logged in successfully" });
 	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
+		//res.status(500).send({ message: "Internal Server Error" });
 	}
 });
 
