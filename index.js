765afd05b09b809da90372232160c3552d566111
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-09 22:12:27 -0300

    nm

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Conn = require('./conn/conn');
+const Conn = require("./conn/conn");
 
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
