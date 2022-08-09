c01fbf6eb8bd350a6e6c74dd5627a5d380574103
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-08-08 21:28:56 -0300

    g

--- index.js
+++ index.js
@@ -3,12 +3,49 @@ if(process.env.NODE_ENV !== 'production') {
 }
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const Conn = require("./conn/conn");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-const ProdutosRoutes = require('./routes/produtos.routes');
 
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var cors = require('cors');
+var multer = require('multer'),
+  bodyParser = require('body-parser'),
+  path = require('path');
+var mongoose = require("mongoose");
+var fs = require('fs');
+var product = require("./models/product.js");
+
+
+var dir = './uploads';
+var upload = multer({
+  storage: multer.diskStorage({
+
+    destination: function (req, file, callback) {
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      callback(null, './uploads');
+    },
+    filename: function (req, file, callback) { callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); }
+
+  }),
+
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname)
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+      return callback(/*res.end('Only images are allowed')*/ null, false)
+    }
+    callback(null, true)
+  }
+});
+app.use(cors());
+app.use(express.static('uploads'));
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: false
+}));
 
 
 
@@ -19,8 +56,193 @@ app.use(cors());
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/produtos", ProdutosRoutes);
 
+/* Api to add Product */
+app.post("/add-product", upload.any(), (req, res) => {
+  try {
+    if (req.files && req.body && req.body.name && req.body.desc && req.body.price &&
+      req.body.discount) {
+
+      let new_product = new product();
+      new_product.name = req.body.name;
+      new_product.desc = req.body.desc;
+      new_product.price = req.body.price;
+      new_product.image = req.files[0].filename;
+      new_product.discount = req.body.discount;
+      new_product.user_id = req.user.id;
+      new_product.save((err, data) => {
+        if (err) {
+          res.status(400).json({
+            errorMessage: err,
+            status: false
+          });
+        } else {
+          res.status(200).json({
+            status: true,
+            title: 'Product Added successfully.'
+          });
+        }
+      });
+
+    } else {
+      res.status(400).json({
+        errorMessage: 'Add proper parameter first!',
+        status: false
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      errorMessage: 'Something went wrong!',
+      status: false
+    });
+  }
+});
+
+/* Api to update Product */
+app.post("/update-product", upload.any(), (req, res) => {
+  try {
+    if (req.files && req.body && req.body.name && req.body.desc && req.body.price &&
+      req.body.id && req.body.discount) {
+
+      product.findById(req.body.id, (err, new_product) => {
+
+        // if file already exist than remove it
+        if (req.files && req.files[0] && req.files[0].filename && new_product.image) {
+          var path = `./uploads/${new_product.image}`;
+          fs.unlinkSync(path);
+        }
+
+        if (req.files && req.files[0] && req.files[0].filename) {
+          new_product.image = req.files[0].filename;
+        }
+        if (req.body.name) {
+          new_product.name = req.body.name;
+        }
+        if (req.body.desc) {
+          new_product.desc = req.body.desc;
+        }
+        if (req.body.price) {
+          new_product.price = req.body.price;
+        }
+        if (req.body.discount) {
+          new_product.discount = req.body.discount;
+        }
+
+        new_product.save((err, data) => {
+          if (err) {
+            res.status(400).json({
+              errorMessage: err,
+              status: false
+            });
+          } else {
+            res.status(200).json({
+              status: true,
+              title: 'Product updated.'
+            });
+          }
+        });
+
+      });
+
+    } else {
+      res.status(400).json({
+        errorMessage: 'Add proper parameter first!',
+        status: false
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      errorMessage: 'Something went wrong!',
+      status: false
+    });
+  }
+});
+
+/* Api to delete Product */
+app.post("/delete-product", (req, res) => {
+  try {
+    if (req.body && req.body.id) {
+      product.findByIdAndUpdate(req.body.id, { is_delete: true }, { new: true }, (err, data) => {
+        if (data.is_delete) {
+          res.status(200).json({
+            status: true,
+            title: 'Product deleted.'
+          });
+        } else {
+          res.status(400).json({
+            errorMessage: err,
+            status: false
+          });
+        }
+      });
+    } else {
+      res.status(400).json({
+        errorMessage: 'Add proper parameter first!',
+        status: false
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      errorMessage: 'Something went wrong!',
+      status: false
+    });
+  }
+});
+
+/*Api to get and search product with pagination and search by name*/
+app.get("/get-product", (req, res) => {
+  try {
+    var query = {};
+    query["$and"] = [];
+    query["$and"].push({
+      is_delete: false,
+      user_id: req.user.id
+    });
+    if (req.query && req.query.search) {
+      query["$and"].push({
+        name: { $regex: req.query.search }
+      });
+    }
+    var perPage = 5;
+    var page = req.query.page || 1;
+    product.find(query, { date: 1, name: 1, id: 1, desc: 1, price: 1, discount: 1, image: 1 })
+      .skip((perPage * page) - perPage).limit(perPage)
+      .then((data) => {
+        product.find(query).count()
+          .then((count) => {
+
+            if (data && data.length > 0) {
+              res.status(200).json({
+                status: true,
+                title: 'Product retrived.',
+                products: data,
+                current_page: page,
+                total: count,
+                pages: Math.ceil(count / perPage),
+              });
+            } else {
+              res.status(400).json({
+                errorMessage: 'There is no product!',
+                status: false
+              });
+            }
+
+          });
+
+      }).catch(err => {
+        res.status(400).json({
+          errorMessage: err.message || err,
+          status: false
+        });
+      });
+  } catch (e) {
+    res.status(400).json({
+      errorMessage: 'Something went wrong!',
+      status: false
+    });
+  }
+
+});
 
 //ssssssssssss
 
