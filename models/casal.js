381fb140ebee87083dd2d1bd933e88ecb8c0dc95
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-09-19 09:28:52 -0300

    m

--- models/casal.js
+++ models/casal.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 casalSchema = new Schema( {
-	name: String,
-	desc: String,
-	niverH: Date,
-	image: String,
-	niverM: Date,
-	tel: Number,
+	name: { type: String, required: true },
+	desc: { type: String, required: true },
+	niverH:{ type: Date, required: true },
+	niverM: { type: Date, required: true },
+	image: { type: String, required: true },
+	tel: { type: Number, required: true },
 	user_id: Schema.ObjectId,
 	is_delete: { type: Boolean, default: false },
 	date : { type : Date, default: Date.now }
